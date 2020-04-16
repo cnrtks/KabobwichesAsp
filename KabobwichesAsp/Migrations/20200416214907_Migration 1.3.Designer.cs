da29e809813a7a1aaf82426a95c2c09578793468
cnrtks <cnrtks@gmail.com>
2020-04-16 17:50:48 -0400

    fixed merge issues

--- KabobwichesAsp/Migrations/20200416214907_Migration 1.3.Designer.cs
+++ KabobwichesAsp/Migrations/20200416214907_Migration 1.3.Designer.cs
@@ -4,14 +4,16 @@ using KabobwichesAsp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KabobwichesAsp.Migrations
 {
     [DbContext(typeof(Repository))]
-    partial class RepositoryModelSnapshot : ModelSnapshot
+    [Migration("20200416214907_Migration 1.3")]
+    partial class Migration13
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
